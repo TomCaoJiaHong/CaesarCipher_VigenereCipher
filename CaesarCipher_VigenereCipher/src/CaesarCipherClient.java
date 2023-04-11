3508f9a8022d57e44e8d5988c89b7735b083232f
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-11 14:54:31 -0300

    create CaesarCipherClient file
    create the file and set variables

---
+++ CaesarCipher_VigenereCipher/src/CaesarCipherClient.java
@@ -0,0 +1,14 @@
+//Author: Jiahong Cao
+
+import java.io.*;
+import java.net.*;
+
+public class CaesarCipherClient {
+    public static void main(String[] args) throws IOException{
+        //Initialize a socket object that we call link, and a PrintWriter object that
+        //we call output, and a BufferedReader object that we call input
+        Socket link = null;
+        PrintWriter output = null;
+        BufferedReader input = null;
+    }
+}
