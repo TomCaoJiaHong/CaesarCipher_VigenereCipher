a1298a2809f4cead6f427d6e386527874f64dfda
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-13 14:13:02 -0300

    Update CaesarCipherServer.java
    create a serverSock object

--- CaesarCipher_VigenereCipher/src/CaesarCipherServer.java
+++ CaesarCipher_VigenereCipher/src/CaesarCipherServer.java
@@ -6,6 +6,18 @@ import java.util.Random;
 
 public class CaesarCipherServer {
     public static void main(String[] args) throws IOException{
-
+        //create a serverSock object
+        ServerSocket serverSock = null;
+        //Instantiate the ServerSocket object on a port. We can choose any port
+        //from 1024-65535, since we are creating our own “toy” server. The client will
+        //send its messages to this port
+        try{
+            serverSock = new ServerSocket(50000);
+        }
+        //in case the connection was unsuccessful
+        catch (IOException ie){
+            System.out.println("Can't listen on 50000");
+            System.exit(1);
+        }
     }
 }
