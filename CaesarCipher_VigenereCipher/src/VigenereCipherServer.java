c8aff866590cc7999767bbe42a4b509d38fb6601
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-17 15:28:53 -0300

    Update VigenereCipherServer.java
    Initialize a socket object that we call link

--- CaesarCipher_VigenereCipher/src/VigenereCipherServer.java
+++ CaesarCipher_VigenereCipher/src/VigenereCipherServer.java
@@ -6,5 +6,11 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class VigenereCipherServer {
-
+    public static void main(String[] args) throws IOException{
+        //Initialize a socket object that we call link, and a PrintWriter object that
+        //we call output, and a BufferedReader object that we call input
+        Socket link = null;
+        PrintWriter output = null;
+        BufferedReader input = null;
+    }
 }
