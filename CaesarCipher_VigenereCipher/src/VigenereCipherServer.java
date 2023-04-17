16edf41867169e06f9cd89b87e3e694ed18c8ef2
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-17 15:29:25 -0300

    Update VigenereCipherServer.java
    create the link object

--- CaesarCipher_VigenereCipher/src/VigenereCipherServer.java
+++ CaesarCipher_VigenereCipher/src/VigenereCipherServer.java
@@ -12,5 +12,26 @@ public class VigenereCipherServer {
         Socket link = null;
         PrintWriter output = null;
         BufferedReader input = null;
+        //then the client has to create the link object, which uses the local
+        //machine IP address as the destination (127.0.0.1), and the port number that the
+        //server is looking for, which is 50000. Since we are testing both the client and
+        //the server programs on the same machine, you should use 127.0.0.1 as the
+        //destination IP address. An IP address that starts with 127 is called a loop-back
+        //address and the packet doesn’t leave the machine
+        try{
+            link = new Socket("127.0.0.1", 50000);
+            output = new PrintWriter(link.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(link.getInputStream()));
+        }
+        //Catch exceptions if the above doesn’t work
+        catch(UnknownHostException e)
+        {
+            System.out.println("Unknown Host");
+            System.exit(1);
+        }
+        catch (IOException e){
+            System.out.println("Cannot connect to host");
+            System.exit(1);
+        }
     }
 }
